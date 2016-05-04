e1707eebe012978516c8e03452ee3d2b46e2edd2
Sean Keegan <seanryankeegan@gmail.com>
2016-05-04 18:53:32 -0400

    search totally redone thanks materialize

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,59 +1,36 @@
 import React from 'react';
 import display from '../styles/styles';
-import {Navbar, NavItem} from 'react-materialize';
+import {Input, Row, Button} from 'react-materialize';
 
 
 const Search = React.createClass({
   render: function(){
     return(
       <div>
-        <hr/>
         <h3>Search for a video!</h3>
-        <div className="filter-bar">
-          <form style={display.search.parentDiv}>
-            <div className="form-element">
-              <p style={display.search.searchPara} class="search-paragraph">Enter a city*</p>
-              <input type="text" id="city-input" onChange={this.props.onChangeCity}></input>
-            </div>
 
-            <div className="form-element">
-              <p style={display.search.searchPara} class="search-paragraph">Search radius(mi)?</p>
-              <input id="radius-input" onChange={this.props.onChangeRadius}></input>
-            </div>
-
-            <div className="form-element">
-              <p style={display.search.searchPara} class="search-paragraph">All videos or only live?</p>
-              <select
-                className="select-live"
-                onChange = {this.props.onChangeLive}
-                >
-                <option value="false">All</option>
-                <option value="true">Only live</option>
-              </select>
-            </div>
-
-            <div className="form-element">
-              <p style={display.search.searchPara} class="search-paragraph">How many results would you like?</p>
-              <select
-                className="select-number"
-                style={display.search.searchSelect}
-                onChange={this.props.onChangeMaxResults}
-                >
-                <option value="5">5</option>
-                <option value="10">10</option>
-                <option value="25">25</option>
-                <option value="50">50</option>
-              </select>
-            </div>
-
-            <div className="form-element">
-              <p style={display.search.searchPara} class="search-paragraph">Anything in particular you'd like to search for?</p>
-              <input id="query-input" onChange={this.props.onChangeQuery}></input>
-            </div>
-          </form>
-        </div>
-        <button style={display.search.button}type='button' onClick={this.props.onSubmit}>Search!</button>
-        <hr/>
+          <Row>
+            <Input s={2} onChange={this.props.onChangeCity} label="Search Location" placeholder="ex: 'New York,' 'Russia'" />
+            <Input s={1.5} type="select" onChange = {this.props.onChangeLive} label="Live or All">
+              <option value="false">All videos</option>
+              <option value="true">Only live</option>
+            </Input>
+            <Input s={1.5} type="select" onChange={this.props.onChangeRadius} label ="Search Radius">
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="25">25</option>
+              <option value="50">50</option>
+            </Input>
+            <Input s={1.5} type="select" onChange={this.props.onChangeMaxResults} label ="# of videos returned?">
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="25">25</option>
+              <option value="50">50</option>
+            </Input>
+            <Input s={3} onChange={this.props.onChangeQuery} label ="Anything in particular you'd like to find?"/>
+            <Button floating large className='red' waves='light' icon='grade' onClick={this.props.onSubmit} />
+          </Row>
+        <hr></hr>
       </div>
     )
   }

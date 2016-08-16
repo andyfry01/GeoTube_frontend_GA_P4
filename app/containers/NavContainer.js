513a80a42f2a23ff9bb8849a373501fb9e266b6a
Andy Fry <andyfry01@gmail.com>
2016-08-15 21:07:56 -0400

    minor styling

--- app/containers/NavContainer.js
+++ app/containers/NavContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Nav from '../components/Nav';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
+import display from '../styles/styles'
 
 const NavContainer = React.createClass({
   render(){
     return(
-      <Nav style={null}/>
+      <Nav style={display.nav}/>
     )
   }
 })
--- app/styles/styles.js
+++ app/styles/styles.js
@@ -1,4 +1,10 @@
 export default {
+  nav: {
+    height: '300px',
+    color: 'purple',
+    fontSize: '20px',
+    border: '1px solid black'
+  },
   main: {
     searchContainer: null
 
